d774e408d9a02461acd7ec3aa9d2d202f79a9f8e
cwilhit@purdue.edu <pr3t3r1st>
2016-10-03 15:47:42 -0400

    Added in GPS updates.

--- app/src/main/java/cs490/frame/WorldController.java
+++ app/src/main/java/cs490/frame/WorldController.java
@@ -1,8 +1,25 @@
 package cs490.frame;
 
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +27,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class WorldController extends FragmentActivity implements OnMapReadyCallback {
+public class WorldController extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener
+{
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleClient;
+
+    private LocationRequest mLocationRequest;
+
+    private static final int REQUEST_CHECK_SETTINGS = 100;
+    private static final int REQUEST_ACCESS_FINE_LOC = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,8 +48,159 @@ public class WorldController extends FragmentActivity implements OnMapReadyCallb
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        createLocationRequest();
+        connectToGoogleApi();
     }
 
+    @Override
+    protected void onStart()
+    {
+        mGoogleClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        mGoogleClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if(mGoogleClient.isConnected())
+            startLocationUpdates();
+        else
+            connectToGoogleApi();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHints)
+    {
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int status)
+    {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result)
+    {
+        Log.d("Connection Error", result.getErrorMessage());
+    }
+
+    private void startLocationUpdates()
+    {
+        try {
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                    .addLocationRequest(mLocationRequest);
+
+            PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(mGoogleClient, builder.build());
+            result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+                @Override
+                public void onResult(LocationSettingsResult result) {
+                    final Status status = result.getStatus();
+                    switch (status.getStatusCode()) {
+                        case LocationSettingsStatusCodes.SUCCESS:
+                            setLocationListener();
+                            break;
+
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            // Location settings are not satisfied. But could be fixed by showing the user a dialog.
+                            try {
+                                // Show the dialog by calling startResolutionForResult(),
+                                // and check the result in onActivityResult().
+                                status.startResolutionForResult(WorldController.this, REQUEST_CHECK_SETTINGS);
+                            } catch (IntentSender.SendIntentException e) {
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            // Location settings are not satisfied. However, we have no way to fix the
+                            // settings so we won't animateIn the dialog.
+                            break;
+                    }
+                }
+            });
+        }
+        catch(SecurityException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void setLocationListener()
+    {
+        //Check for permissions
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_ACCESS_FINE_LOC);
+
+            return;
+        }
+
+        try {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleClient, mLocationRequest, this);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    @SuppressWarnings({"MissingPermission"})
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_ACCESS_FINE_LOC: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleClient, mLocationRequest, this);
+                } else {
+                    // permission denied. Kill the app.
+                    finish();
+                }
+                return;
+            }
+        }
+    }
+
+    private void createLocationRequest()
+    {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void connectToGoogleApi()
+    {
+        if(mGoogleClient == null)
+        {
+            mGoogleClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Add a marker in Sydney and move the camera
+        LatLng user = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(user).title("Person Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(user));
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,10 +214,5 @@ public class WorldController extends FragmentActivity implements OnMapReadyCallb
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
