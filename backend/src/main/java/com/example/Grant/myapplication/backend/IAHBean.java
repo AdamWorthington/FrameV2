58ab4ee70a5170f8069923d3190f28d4354c28d5
Daniel Sokoler <dsokoler@purdue.edu>
2016-10-06 11:51:11 -0400

    Updated SQL/API Files

--- backend/src/main/java/com/example/Grant/myapplication/backend/IAHBean.java
+++ backend/src/main/java/com/example/Grant/myapplication/backend/IAHBean.java
@@ -1,17 +1,19 @@
 package com.example.Grant.myapplication.backend;
 
+import java.util.ArrayList;
+
 /**
  * Created by Daniel on 10/6/2016.
  */
 
 public class IAHBean {
-    private SQLStatements.ImageAttributeHolder iah;
+    private ArrayList<SQLStatements.ImageAttributeHolder> iah;
 
-    public SQLStatements.ImageAttributeHolder getData() {
+    public ArrayList<SQLStatements.ImageAttributeHolder> getData() {
         return iah;
     }
 
-    public void setData(SQLStatements.ImageAttributeHolder iah) {
+    public void setData(ArrayList<SQLStatements.ImageAttributeHolder> iah) {
         iah = iah;
     }
 }
