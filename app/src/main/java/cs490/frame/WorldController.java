40ba91149d5e1e64d69fad4f5a04916976b1cdce
Chazuke <smcclel@purdue.edu>
2016-12-04 04:21:17 -0500

    Did notifications for posts that enter your viewable range while moving around

--- app/src/main/java/cs490/frame/WorldController.java
+++ app/src/main/java/cs490/frame/WorldController.java
@@ -1,5 +1,9 @@
 package cs490.frame;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
@@ -8,6 +12,7 @@ import android.location.Location;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -41,8 +46,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.ArrayList;
 
 public class WorldController extends AppCompatActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener
-{
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap;
     private GoogleApiClient mGoogleClient;
@@ -52,6 +56,7 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
     private static final int REQUEST_CHECK_SETTINGS = 100;
     private static final int REQUEST_ACCESS_FINE_LOC = 200;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private static final int viewRange = 50; //Distance from user where posts are visible (in meters)
     private int first = 0;
     private String displayName;
 
@@ -59,10 +64,12 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
     static Location curLoc;
 
     private Circle circle;
+    private ArrayList<ImageAttributeHolder> posts = null;
+    private int[] seen = null;
+    private boolean inForeground;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.world_view);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -86,49 +93,45 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
     }
 
     @Override
-    protected void onStart()
-    {
+    protected void onStart() {
         mGoogleClient.connect();
         super.onStart();
+        inForeground = true;
     }
 
     @Override
-    protected void onStop()
-    {
+    protected void onStop() {
         mGoogleClient.disconnect();
         super.onStop();
+        inForeground = false;
     }
 
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
-        if(mGoogleClient.isConnected())
+        if (mGoogleClient.isConnected())
             startLocationUpdates();
         else
             connectToGoogleApi();
+        inForeground = true;
     }
 
     @Override
-    public void onConnected(Bundle connectionHints)
-    {
+    public void onConnected(Bundle connectionHints) {
         startLocationUpdates();
     }
 
     @Override
-    public void onConnectionSuspended(int status)
-    {
+    public void onConnectionSuspended(int status) {
 
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult result)
-    {
+    public void onConnectionFailed(ConnectionResult result) {
         Log.d("Connection Error", result.getErrorMessage());
     }
 
-    private void startLocationUpdates()
-    {
+    private void startLocationUpdates() {
         try {
             LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
                     .addLocationRequest(mLocationRequest);
@@ -160,19 +163,15 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
                     }
                 }
             });
-        }
-        catch(SecurityException e)
-        {
+        } catch (SecurityException e) {
             e.printStackTrace();
         }
     }
 
-    private void setLocationListener()
-    {
+    private void setLocationListener() {
         //Check for permissions
-        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED)
-        {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_ACCESS_FINE_LOC);
 
@@ -181,9 +180,7 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
 
         try {
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleClient, mLocationRequest, this);
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -206,18 +203,15 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
         }
     }
 
-    private void createLocationRequest()
-    {
+    private void createLocationRequest() {
         mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(5000);
-        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setFastestInterval(5000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
-    private void connectToGoogleApi()
-    {
-        if(mGoogleClient == null)
-        {
+    private void connectToGoogleApi() {
+        if (mGoogleClient == null) {
             mGoogleClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
@@ -230,7 +224,7 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
     public void onLocationChanged(Location location) {
         curLoc = location;
 
-        //move camera on only the first location
+        //Center camera on user location only the first time the map is created
         if (first == 0) {
             LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
             mMap.animateCamera(CameraUpdateFactory.newLatLng(loc));
@@ -241,13 +235,19 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
         if (circle == null) {
             circle = mMap.addCircle(new CircleOptions()
                     .center(new LatLng(location.getLatitude(), location.getLongitude()))
-                    .radius(50)
+                    .radius(viewRange)
                     .strokeColor(Color.RED)
                     .fillColor(Color.TRANSPARENT)
                     .visible(true)
             );
         } else {
             circle.setCenter(new LatLng(location.getLatitude(), location.getLongitude()));
+        }
+
+        //Check to see if new posts have come into user's location
+        //If statement to be used for settings in future when feature can be disabled
+        if (true) {
+            checkNewPosts();
         }
     }
 
@@ -271,26 +271,11 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
             @Override
             public void onInfoWindowClick(Marker marker) {
                 //Check to see if marker is in radius
-                LatLng markerLatLng = marker.getPosition();
-                Location there = new Location("");
-                there.setLatitude(markerLatLng.latitude);
-                there.setLongitude(markerLatLng.longitude);
-                if (curLoc.distanceTo(there) <= 50) {
-                    ImageBean image = null;
-                    try {
-                        image = new GetImage().execute(Integer.parseInt(marker.getTitle())).get();
-                        if (image.getInfo() != null)
-                            if (image.getInfo().equals("Connection Failure in getImage")) Toast.makeText(WorldController.this, "Failed to retrieve image from server", Toast.LENGTH_SHORT).show();
-                        if (image.getInfo() == null) {
-                            Intent imageView = new Intent(WorldController.this, DisplayImageActivity.class);
-                            showPicture = image.getData();
-                            startActivity(imageView);
-                        }
-                    } catch (Exception e) {
-                        Log.e("WorldController", e.getMessage());
-                        Toast.makeText(WorldController.this, "Failed to retrieve image from server", Toast.LENGTH_SHORT).show();
-                    }
+                if (inRange(marker)) {
+                    //Marker in range, display post
+                    displayPost(marker.getTitle());
                 } else {
+                    //Marker not in range, display err
                     Toast.makeText(WorldController.this, "Not close enough to marker", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -302,7 +287,6 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
         enableMyLocation();
 
         //Get all posts from the db
-        ArrayList<ImageAttributeHolder> posts = null;
         try {
             posts = new GetAllAttributes().execute().get();
         } catch (Exception e) {
@@ -318,6 +302,7 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
                 String snippet = "Posted by: " + holder.getUser();
                 addMapMarker(position, title, snippet);
             }
+            seen = new int[posts.size()];
         } else {
             Toast.makeText(this, "Error retrieving posts from server", Toast.LENGTH_SHORT).show();
         }
@@ -326,10 +311,11 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
 
     public void addMapMarker(LatLng position, String title, String snippet) {
         mMap.addMarker(new MarkerOptions()
-            .position(position)
-            .title(title)
-            //.icon()
-            .snippet(snippet));
+                .position(position)
+                .title(title)
+                //For future use when custom icon is created
+                //.icon()
+                .snippet(snippet));
     }
 
     private void enableMyLocation() {
@@ -344,9 +330,103 @@ public class WorldController extends AppCompatActivity implements OnMapReadyCall
         }
     }
 
-    private void onCameraClick()
-    {
+    private void onCameraClick() {
         Intent cameraIntent = new Intent(this, CameraActivity.class);
         startActivity(cameraIntent);
+    }
+
+    //Checks the list of posts to see if the user has any posts within viewable range
+    //If there are, mark them as seen and notify the user
+    //Will run through all posts pulled from the server and will bog down runtime if too many posts
+    //Stop-Gap: increase interval between new location updates
+    //Potential Long-Term: Async task
+    private void checkNewPosts() {
+        //No posts, err retrieving posts, or no user location: do nothing
+        if (posts == null || curLoc == null) {
+            return;
+        }
+
+        //Check to see if any new posts have come into viewable rangeS
+        int newPosts = 0;
+        for(int i = 0; i < posts.size(); i++) {
+            if (seen[i] != 1) {
+                ImageAttributeHolder iah = posts.get(i);
+                LatLng post = new LatLng(iah.getLatitude(), iah.getLongitude());
+                Location there = new Location("");
+                there.setLatitude(post.latitude);
+                there.setLongitude(post.longitude);
+                //If post has come into view range, mark it seen, increment counter
+                if (curLoc.distanceTo(there) <= viewRange) {
+                    seen[i] = 1;
+                    newPosts++;
+                }
+            }
+        }
+
+        //Notify user if any new posts have come into viewable range
+        if (newPosts > 0 && !inForeground) {
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(WorldController.this)
+                            .setSmallIcon(R.drawable.cast_ic_notification_small_icon)
+                            .setContentTitle("Frame")
+                            .setContentText(newPosts + " new posts in your vicinity!");
+            // Creates an explicit intent for an Activity in your app
+            Intent resultIntent = new Intent(WorldController.this, WorldController.class);
+
+            // The stack builder object will contain an artificial back stack for the
+            // started Activity.
+            // This ensures that navigating backward from the Activity leads out of
+            // your application to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(WorldController.this);
+            // Adds the back stack for the Intent (but not the Intent itself)
+            stackBuilder.addParentStack(WorldController.class);
+            // Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            // mId allows you to update the notification later on.
+            int mId = 0;
+            mNotificationManager.notify(mId, mBuilder.build());
+        }
+    }
+
+
+    //Checks if the user is within viewable distance from a marker
+    private boolean inRange(Marker marker) {
+        //Check to see if marker is in radius
+        LatLng markerLatLng = marker.getPosition();
+        Location there = new Location("");
+        there.setLatitude(markerLatLng.latitude);
+        there.setLongitude(markerLatLng.longitude);
+        if (curLoc.distanceTo(there) <= viewRange) {
+           return true;
+        }
+        else return false;
+    }
+
+    //Retrieves the post content from the server and switches views to display it
+    //Only to be called after checking if post is within viewable range
+    private void displayPost(String title) {
+        ImageBean image = null;
+        try {
+            image = new GetImage().execute(Integer.parseInt(title)).get();
+            if (image.getInfo() != null)
+                if (image.getInfo().equals("Connection Failure in getImage"))
+                    Toast.makeText(WorldController.this, "Failed to retrieve image from server", Toast.LENGTH_SHORT).show();
+            if (image.getInfo() == null) {
+                Intent imageView = new Intent(WorldController.this, DisplayImageActivity.class);
+                showPicture = image.getData();
+                startActivity(imageView);
+            }
+        } catch (Exception e) {
+            Log.e("WorldController", e.getMessage());
+            Toast.makeText(WorldController.this, "Failed to retrieve image from server", Toast.LENGTH_SHORT).show();
+        }
     }
 }
