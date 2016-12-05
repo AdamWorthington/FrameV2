8ac4052f791efe54f369442b37fcf393dfc3b556
Daniel Sokoler <dsokoler@purdue.edu>
2016-12-05 14:13:40 -0500

    Created Comment class

--- backend/src/main/java/com/example/Grant/myapplication/backend/Comment.java
+++ backend/src/main/java/com/example/Grant/myapplication/backend/Comment.java
@@ -5,4 +5,38 @@ package com.example.Grant.myapplication.backend;
  */
 
 public class Comment {
+
+    int postID;
+    String comment;
+    String user;
+
+    public Comment (int postID, String comment, String user) {
+        this.postID = postID;
+        this.comment = comment;
+        this.user = user;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int getPostID() {
+        return postID;
+    }
+
+    public void setPostID(int postID) {
+        this.postID = postID;
+    }
 }
